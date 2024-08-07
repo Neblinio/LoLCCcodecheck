e42c3bf29aca6542e2f014e34abb8c572ba45951
Neblinio <neblinio@gmail.com>
2024-08-07 08:01:10 +0200

    Add files via upload

--- codecheck.py
+++ codecheck.py
@@ -1,6 +1,7 @@
 import asyncio
 import requests
 import json
+import os
 
 async def check(bearer_token, code):
     url = "https://usw2-red.pp.sgp.pvp.net/contentcodes/v1/validate"
@@ -21,12 +22,32 @@ async def check(bearer_token, code):
         return(response_data["status"])
     
 async def main_async():
-    token = str(input("Input Bearer token: "))
-    num_times = int(input("How many codes do you want to check? "))
-    for _ in range(num_times):
-        cc = str(input("Input CC code: "))
-        contents = await check(token, cc)
-        print(contents)
+    if os.path.isfile("bearertoken.txt") and os.stat("bearertoken.txt").st_size != 0:
+        myfile = open("bearertoken.txt",'r')
+        token = (myfile.read())
+        myfile.close()
+    else:
+        token = str(input("Input Bearer token: "))
+        
+    if os.path.isfile("cccodes.txt") and os.stat("cccodes.txt").st_size != 0:
+        myfile = open("cccodes.txt",'r')
+        for line in myfile:
+            cc = (line.strip('\n'))
+            print("Checking:",cc)
+            contents = await check(token, cc)
+            print(contents)
+            while contents == ("\'message\': \'Too Many Requests\', \'status_code\': 429"):
+                print("Retrying:",cc)
+                contents = await check(token, cc)
+                print(contents)
+            await asyncio.sleep(7)    
+        myfile.close()
+    else:
+        num_times = int(input("How many codes do you want to check? "))
+        for _ in range(num_times):
+            cc = str(input("Input CC code: "))
+            contents = await check(token, cc)
+            print(contents)
     
 if __name__ == "__main__":
     asyncio.run(main_async())
